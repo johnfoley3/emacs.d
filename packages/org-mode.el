af9556fcb10d62da0f749bbddf0b919a5a9c6dd8
John Foley <johnfoley3@gmail.com>
2017-11-13 12:22:26 -0600

    Adds org mode package location directly from orgmode.org

--- packages/org-mode.el
+++ packages/org-mode.el
@@ -1,12 +1,11 @@
 ;;; custom org mode configuration
-
 (defun foley/build-jira-link (id)
   "Given a Jira ID number and optional project PREFIX, return the url of the Jira issue."
   (interactive "sJira ID: ")
   (insert (concat "https://intoximeters.atlassian.net/browse/TRACK-" id)))
 
-(add-hook 'org-mode-hook
-          (lambda ()
-            (local-set-key "\M-i" 'org-clock-in)
-            (local-set-key "\M-o" 'org-clock-out)
-            (local-set-key "\M-J" 'foley/build-jira-link)))
+(use-package org-mode
+  :bind (:map org-mode-map
+              ("M-i" . 'org-clock-in)
+              ("M-o" . 'org-clock-out)
+              ("M-J" . 'foley/build-jira-link)))
