1155b99e402a2d6ce7ef70d156ec30f7a484f9dd
John Foley <johnfoley3@gmail.com>
2017-11-13 12:00:01 -0600

    Adds various and knicknacks; adds rschema snippets

--- packages/org-mode.el
+++ packages/org-mode.el
@@ -1,5 +1,12 @@
 ;;; custom org mode configuration
+
+(defun foley/build-jira-link (id)
+  "Given a Jira ID number and optional project PREFIX, return the url of the Jira issue."
+  (interactive "sJira ID: ")
+  (insert (concat "https://intoximeters.atlassian.net/browse/TRACK-" id)))
+
 (add-hook 'org-mode-hook
           (lambda ()
             (local-set-key "\M-i" 'org-clock-in)
-            (local-set-key "\M-o" 'org-clock-out)))
+            (local-set-key "\M-o" 'org-clock-out)
+            (local-set-key "\M-J" 'foley/build-jira-link)))
