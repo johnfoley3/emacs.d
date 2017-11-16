d9c5babdacf6c128f82ea354f682076fec56416a
John Foley <johnfoley3@gmail.com>
2017-11-16 08:38:07 -0600

    Fixes issue where key mapping throws errors on start

--- packages/org-mode.el
+++ packages/org-mode.el
@@ -4,8 +4,4 @@
   (interactive "sJira ID: ")
   (insert (concat "https://intoximeters.atlassian.net/browse/TRACK-" id)))
 
-(use-package org-mode
-  :bind (:map org-mode-map
-              ("M-i" . 'org-clock-in)
-              ("M-o" . 'org-clock-out)
-              ("M-J" . 'foley/build-jira-link)))
+(use-package org)
