baeea38f49be76d1aa151e6f7eb12bcf271ae7a3
John Foley <johnfoley3@gmail.com>
2017-12-12 15:52:51 -0600

    Adds tide support for tsx files

--- packages/flycheck.el
+++ packages/flycheck.el
@@ -3,7 +3,7 @@
   :init (global-flycheck-mode))
 
 (defun foley-flycheck-command-wrapper (command)
-  "Configured with Flycheck to receive a command that a checker builds and return
- the actual command to run"
+  "Configured with Flycheck to receive a COMMAND that a checker builds
+and return the actual command to run"
   (if (string= (car command) "rubocop")
       (append '("bundle" "exec") command)))
--- packages/tide.el
+++ packages/tide.el
@@ -1,5 +1,5 @@
 (use-package tide
-  :ensure t
+  ;; :mode ("\\.tsx\\'" . typescript-mode)
   :init
   (add-hook 'typescript-mode-hook
           (lambda ()
@@ -10,3 +10,12 @@
             ;; company is an optional dependency. You have to
             ;; install it separately via package-install
             (company-mode +1))))
+
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "tsx" (file-name-extension buffer-file-name))
+              (tide-setup))))
+;; enable typescript-tslint checker
+;; (flycheck-add-mode 'typescript-tslint 'web-mode)
+;; currently adds it to all web mode, and that is just stupid
