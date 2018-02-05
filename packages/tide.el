446218ce5eaacb2faa62c017721960970d2b33a9
John Foley <johnfoley3@gmail.com>
2018-02-05 10:14:20 -0600

    Improve Tide setup and add snippets

--- packages/tide.el
+++ packages/tide.el
@@ -1,21 +1,27 @@
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
 (use-package tide
   ;; :mode ("\\.tsx\\'" . typescript-mode)
   :init
-  (add-hook 'typescript-mode-hook
-          (lambda ()
-            (tide-setup)
-            (flycheck-mode +1)
-            (setq flycheck-check-syntax-automatically '(save mode-enabled))
-            (eldoc-mode +1)
-            ;; company is an optional dependency. You have to
-            ;; install it separately via package-install
-            (company-mode +1))))
+  (add-hook 'typescript-mode-hook #'setup-tide-mode))
 
+;; enable typescript-tslint checker
+;; adds it to all web mode, and that is just stupid
 (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(flycheck-add-mode 'typescript-tslint 'web-mode)
+
 (add-hook 'web-mode-hook
           (lambda ()
-            (when (string-equal "tsx" (file-name-extension buffer-file-name))
-              (tide-setup))))
-;; enable typescript-tslint checker
-;; (flycheck-add-mode 'typescript-tslint 'web-mode)
-;; currently adds it to all web mode, and that is just stupid
+            (if (string-equal "tsx" (file-name-extension buffer-file-name))
+                (setup-tide-mode)
+              (setq flycheck-disabled-checkers '(typescript-tslint)))))
