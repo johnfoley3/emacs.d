820d872e51352d27294198f698a5fcec799f1cd4
John Foley <johnfoley3@gmail.com>
2018-07-16 18:26:14 -0500

    Add elixir packages

---
+++ packages/elixir.el
@@ -0,0 +1,16 @@
+;;; major mode
+;; https://github.com/elixir-editors/emacs-elixir
+(use-package elixir-mode)
+
+;;; tool integration
+;; https://github.com/tonini/alchemist.el
+(use-package alchemist)
+
+
+;;; flycheck integration
+;; https://github.com/tomekowal/flycheck-mix
+(use-package flycheck-mix)
+
+;;; org babel integration
+;; https://github.com/zweifisch/ob-elixir
+(use-package ob-elixir)
