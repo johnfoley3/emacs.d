40e2b19451571b7ad5f1ad476bb8db4478697f20
John Foley <johnfoley3@gmail.com>
2019-02-01 13:23:54 -0600

    Experiment with automatically committing org notes

--- packages/org-mode.el
+++ packages/org-mode.el
@@ -21,3 +21,16 @@
   (setq org-id-locations-file "~/.emacs.d/.org-id-locations")
   (setq org-brain-visualize-default-choices 'all)
   (setq org-brain-title-max-length 20))
+
+(defun commit-org-message ()
+  (interactive)
+  (let ((template "changes from %s on %s")
+        (host (system-name))
+        (today (format-time-string "%FT%T%z")))
+    (insert (format template host today))))
+
+(defun commit-org-notes ()
+  (interactive)
+  (add-hook 'git-commit-setup-hook 'commit-org-message)
+  (magit-commit)
+  (add-hook 'git-commit-setup-hook 'commit-org-message))
