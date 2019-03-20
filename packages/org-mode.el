89c4f574adeab5b81958fba9f2d9701c5cc21071
John Foley <johnfoley3@gmail.com>
2019-03-20 08:55:08 -0500

    Clean org mode package file
    I don't use org-jira, the jira link I made is good enough for me
    
    The functions for making a git commit message automatically isn't
    working like I hoped it would. Removing to reduce false code

--- packages/org-mode.el
+++ packages/org-mode.el
@@ -19,8 +19,6 @@
                                       (file+datetree "~/projects/org-notes/days.org")
                                       "**** %<%H:%M> %^{description}\n%?"))
 
-(use-package org-jira)
-
 (use-package org-brain
   :init
   (setq org-brain-path "~/org-brain")
@@ -29,18 +27,3 @@
   (setq org-id-locations-file "~/.emacs.d/.org-id-locations")
   (setq org-brain-visualize-default-choices 'all)
   (setq org-brain-title-max-length 20))
-
-(defun commit-org-message ()
-  (interactive)
-  (let ((template "changes from %s on %s")
-        (host (system-name))
-        (today (format-time-string "%FT%T%z")))
-    (insert (format template host today))))
-
-;; is not working yet
-;; git does not recoganize that text was added
-(defun commit-org-notes ()
-  (interactive)
-  (add-hook 'git-commit-setup-hook 'commit-org-message)
-  (magit-commit)
-  (add-hook 'git-commit-setup-hook 'commit-org-message))
