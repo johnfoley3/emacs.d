d7d0b0b4ab4c6cfd55476b145bed3fbcd5223845
John Foley <johnfoley3@gmail.com>
2019-07-19 08:32:52 -0500

    Remove unused packages

--- packages/engine-mode.el
+++
@@ -1,6 +0,0 @@
-(use-package engine-mode
-  :config
-  (progn
-    (defengine google "http://google.com/search?q=%s" :keybinding "g")
-    (defengine melpa "https://melpa.org/#/?q=%s" :keybinding "m")
-    (engine-mode)))
--- packages/go-lang.el
+++
@@ -1,9 +0,0 @@
-(use-package go-mode)
-(use-package company-go)
-
-;; Requires `gore' to be installed
-;; `go get -u github.com/motemen/gore'
-(use-package gorepl-mode
-  :hook (go-mode . gorepl-mode))
-
-(use-package gotest)
